9562a6324c468549871ee03382c00fa5225be31a
Arogundade-Aanuoluwa <110118941+Arogundade-Aanuoluwa@users.noreply.github.com>
2022-08-22 19:16:35 +0100

    Add files via upload

--- class assignment/api.js
+++ class assignment/api.js
@@ -1,8 +1,12 @@
-
+//create a class object
 class api{
+    //make a get request
     async get(url){
+        // await response of the fetch call
         const response = await fetch(url)
+        // Only proceed once its resolved
         const app = await response.json()
+         // only proceed once second promise is resolved
         return app
     }
 }
--- class assignment/app.js
+++ class assignment/app.js
@@ -1,6 +1,10 @@
+
 const hTag = document.querySelector("h1")
+//instantiate api
 const http = new api()
+// set time interval for function to load every 5 sec
 setInterval(() => {
+    // get users
     http.get("https://catfact.ninja/fact")
     .then(data =>hTag.innerHTML = data.fact)
     .catch(err => console.error(err));
